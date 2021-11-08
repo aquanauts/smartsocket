46d4095a1781f2ca0d5174d810ac8519d8a73abb
Ben Rady <ben.rady@aquatic.com>
2021-11-08 13:28:52 -0600

    Basic view test

--- spec/router_spec.js
+++ spec/router_spec.js
@@ -3,10 +3,17 @@
 describe('Router', () => {
     var containerElem;
     var currentHash;
+    var stubSocket;
 
-    function mainView(context) {
+    function testView(context) {
         const view = document.createElement('div');
         view.className = 'main-view'
+        context.subscribe('users');
+        context.onUpdate(function (key, value) {
+            const elem = document.createElement('p');
+            elem.innerHTML = value;
+            view.after(elem);
+        });
         return view;
     }
 
@@ -22,14 +29,16 @@ describe('Router', () => {
     }
 
     beforeEach(() => {
+        stubSocket = {};
         currentHash = '';
         containerElem = document.createElement('div');
         containerElem.id = "container";
         smartsocket.window = {
             onHashChange: jasmine.createSpy('onHashChange'),
-            getHash: function () { return currentHash }
+            getHash: function () { return currentHash },
+            createWebSocket: function () { return stubSocket }
         };
-        smartsocket.onReady(containerElem, {appRoutes: {"": mainView, "#alt": altView}});
+        smartsocket.onReady(containerElem, {appRoutes: {"": testView, "#alt": altView}});
     });
 
     it('shows the default view', async () => {
@@ -41,4 +50,17 @@ describe('Router', () => {
         expect(containerElem.querySelectorAll('.alt-view').length).toEqual(1);
         expect(containerElem.querySelectorAll('.main-view').length).toEqual(0);
     });
+
+    it('can get the current view name', async () => {
+        expect(smartsocket.currentRoute()).toEqual('');
+        triggerHashChange('#alt');
+        expect(smartsocket.currentRoute()).toEqual('#alt');
+    });
+
+    it('routes events to the view when subscribed', async () => {
+        const event = "a/users/brady/email:brady@gmail.com"
+        stubSocket.onmessage({data: event});
+        expect(containerElem.querySelectorAll('p')[0].innerText).toEqual('brady@gmail.com');
+    });
+
 });
--- src/smartsocket.js
+++ src/smartsocket.js
@@ -2,26 +2,42 @@
 
 const smartsocket = (function () {
     function onReady(rootElem, config) {
+        const socket = connect();
         smartsocket.window.onHashChange(function() {
-            showView(rootElem, smartsocket.window.getHash(), config);
+            showView(rootElem, config, socket);
         });
-        showView(rootElem, smartsocket.window.getHash(), config);
+        showView(rootElem, config, socket);
     }
 
-    function showView(rootElem, hash, config) {
-        const context = createContext(config);
-        const hashParts = hash.split('-');
-        const viewName = hashParts[0];
+    function connect() {
+        // TODO URL?
+        const socket = smartsocket.window.createWebSocket();
+        socket.onmessage = function (e) {
+            // TODO Handle malformed keys with missing ':'
+            const event = e.data;
+            const idx = event.indexOf(':');
+            const key = event.slice(0, idx);
+            const value = event.slice(19 + 1);
+            socket.onEvent(key, value);
+        }
+        return socket;
+    }
+
+    function showView(rootElem, config, socket) {
+        const [viewName, opts] = parseHash();
+        const context = createContext(config, opts, socket);
         const viewFn = config.appRoutes[viewName];
         if (viewFn) {
             rootElem.innerHTML = '';
-            rootElem.append(viewFn(context, hashParts.slice(1)));
+            rootElem.append(viewFn(context));
         }
     };
 
-    function getViewName() {
-        const hashParts = smartsocket.window.getHash().split('-');
-        return hashParts[0];
+    function parseHash() {
+        const hash = smartsocket.window.getHash();
+        const hashParts = hash.split('-');
+        const viewName = hashParts[0];
+        return [viewName, {}];
     }
 
     function currentHashState() {
@@ -29,19 +45,30 @@ const smartsocket = (function () {
         return Object.fromQueryString(hashParts.slice(1).join('-'));
     }
 
-    function createContext(elem) {
+    function createContext(elem, opts, socket) {
+        // TODO
         // How do we handle changing subscriptions and view transitions?
+        //      Could send an unsubscribeFromAll message here because we know the view is changing
+        const updateCallbacks = [];
+
+        socket.onEvent = function (key, value) {
+            for (var cb of updateCallbacks) {
+                cb(key, value);
+            }
+        };
         return {
             subscribe: function(entity, id, column) {
-                // Sends the subscription up the websocket to the server
+                // TODO Sends the subscription up the websocket to the server
             },
 
-            onEvent: function(callback) {
-                // Calls back when an event is received. function(key, value)
+            onUpdate: function(callback) {
+                updateCallbacks.push(callback);
             },
 
             send: function(key, value) {
-            }
+                // TODO socket.send
+            },
+            // TODO getOpts() with Object.freeze(opts)
         }
     }
 
@@ -50,7 +77,12 @@ const smartsocket = (function () {
     function setHashState(key, value) {
         const newState = currentHashState();
         newState[key] = value;
-        smartsocket.window.setHash(`${getViewName()}-${Object.toQueryString(newState)}`);
+        smartsocket.window.setHash(`${currentRoute()}-${Object.toQueryString(newState)}`);
+    }
+
+    function currentRoute() {
+        const [viewName, opts] = parseHash();
+        return viewName;
     }
 
     function getHashState(key) {
@@ -58,13 +90,14 @@ const smartsocket = (function () {
     }
 
     function template(name) {
-        // Consider template element
+        // TODO Consider template element
         return $('.templates .' + name).clone();
     }
 
     return {
         onReady: onReady,
         template: template,
+        currentRoute: currentRoute,
         getHashState: getHashState,
         setHashState: setHashState,
     };
@@ -82,6 +115,12 @@ smartsocket.window = (function () {
     function onHashChange(callback) {
         window.onhashchange = callback;
     }
+
+    function createWebSocket(url) {
+        // TODO replace with ReconnectingWebsocket?
+        // TODO Handle websocket disconnect by setting reloadOnClick
+        return new WebSocket(url);
+    };
 
     return {
         getHash: getHash,
