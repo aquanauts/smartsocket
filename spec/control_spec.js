6c095cdc799865392dc2e91c01fa916d698ffcb4
Ben Rady <ben.rady@aquatic.com>
2022-03-29 16:50:04 -0500

    Adding more tests. Encapsulating in a module

--- spec/control_spec.js
+++ spec/control_spec.js
@@ -4,7 +4,6 @@ describe('Control socket', () => {
     var stubSocket
     var stubWindow
     var sentMessages
-    var socket
     var eventListeners
     var intervalFn
     var timeoutFns
@@ -26,75 +25,163 @@ describe('Control socket', () => {
             setInterval: (fn) => { intervalFn = fn },
             setTimeout: (fn, duration) => { timeoutFns[duration] = fn },
             WebSocket: function(url, protocols) {
-                stubSocket = this;
+                stubSocket = this
                 this.close = () => { stubSocket.readyState = WebSocket.CLOSED }
                 this.send = (msg) => { sentMessages.push(JSON.parse(msg)) }
             }
         }
         sentMessages = []
-        socket = control.connect({
-            url: "localhost:1234",
-            window: stubWindow
+    })
+
+    describe('connect', () => {
+        var socket
+
+        beforeEach(() => {
+            socket = control.connect({
+                url: "localhost:1234",
+                window: stubWindow
+            })
+        })
+
+        it('sends tracked properties when connecting', async () => {
+            stubSocket.onopen()
+            expect(sentMessages).toContain({"location.hash":""})
+        })
+
+        it('sends static properties when connecting', async () => {
+            stubSocket.onopen()
+            expect(sentMessages).toContain({'location.host':'localhost:12345'})
+        })
+
+        it('does not send stats until connected', async () => {
+            expect(sentMessages).toEqual([])
+        })
+
+        it('sends tracked properties when they change', async () => {
+            stubSocket.onopen()
+            stubWindow.location.hash = '#newhash'
+            eventListeners['hashchange']()
+            expect(sentMessages).toContain({'location.hash':'#newhash'})
+        })
+
+        it('sends polled properties when connected', async () => {
+            stubSocket.onopen()
+            expect(sentMessages).toContain({'navigator.cookieEnabled':true})
+        })
+
+        it('only sends polled properties when they change', async () => {
+            stubSocket.onopen()
+            sentMessages = []
+            stubWindow.navigator.cookieEnabled = false
+            intervalFn()
+            intervalFn()
+            expect(sentMessages).toEqual([{'navigator.cookieEnabled':false}])
+        })
+
+        it('evaluates any message sent', async () => {
+            stubSocket.onmessage({data: "1+1"})
+            expect(sentMessages).toEqual([{'$_':2}])
+        })
+
+        it('sends an error object if evaluation fails', async () => {
+            stubSocket.onmessage({data: "1+"})
+            let error = sentMessages[0]['$__']
+            expect(error.message).toEqual("Unexpected end of input")
+            expect(error.stack).toContain("WebSocket.ws.onmessage")
         })
     })
 
-    it('provides the URL as a read-only property', async () => {
-        expect(socket.url).toEqual("localhost:1234");
-        expect(Object.getOwnPropertyDescriptor(socket, 'url').writable).toBeFalsy()
-    });
+    describe('internal socket', () => {
+        let socket
+        let callback
 
-    it('tracks the number of reconnect attempts', async () => {
-        expect(socket.reconnectAttempts).toEqual(0);
-        stubSocket.onclose({})
-        timeoutFns[1000](); // 1000ms reconnect timer
-        expect(socket.reconnectAttempts).toEqual(1);
-        expect(Object.getOwnPropertyDescriptor(socket, 'reconnectAttempts').writable).toBeFalsy()
-    });
+        beforeEach(() => {
+            socket = new control.InternalSocket("localhost:1234", stubWindow)
+            callback = jasmine.createSpy('callback')
+        })
 
-    it('sends tracked properties when connecting', async () => {
-        stubSocket.onopen()
-        expect(sentMessages).toContain({"location.hash":""})
+        it('provides the URL as a read-only property', async () => {
+            expect(socket.url).toEqual("localhost:1234")
+            expect(Object.getOwnPropertyDescriptor(socket, 'url').writable).toBeFalsy()
+        })
+
+        it('tracks the number of reconnect attempts', async () => {
+            expect(socket.reconnectAttempts).toEqual(0)
+            stubSocket.onclose({})
+            timeoutFns[1000](); // 1000ms reconnect timer
+            expect(socket.reconnectAttempts).toEqual(1)
+            expect(Object.getOwnPropertyDescriptor(socket, 'reconnectAttempts').writable).toBeFalsy()
+        })
+
+        it('provides the websocket protocol', async () => {
+            stubSocket.protocol = 'https'
+            stubSocket.onopen()
+            expect(socket.protocol).toEqual('https')
+            expect(Object.getOwnPropertyDescriptor(socket, 'protocol').writable).toBeFalsy()
+        })
+
+        it('fires a closing event when closing', async () => {
+            socket.addEventListener('close', callback)
+            stubSocket.onclose({})
+            expect(callback).toHaveBeenCalled()
+        })
+
+        it('fires an event when a message is received', async () => {
+            socket.addEventListener('message', callback)
+            stubSocket.onmessage({data: "my data"})
+            expect(callback).toHaveBeenCalled()
+            const event = callback.calls.argsFor(0)[0]
+            expect(event.data).toEqual("my data")
+        })
+
+        it('fires an event when an error occurs', async () => {
+            socket.addEventListener('close', callback)
+            stubSocket.onclose({})
+            expect(callback).toHaveBeenCalled()
+        })
+
+        it('fires a connecting event when closing', async () => {
+            socket.addEventListener('connecting', callback)
+            stubSocket.onclose({code: 42, reason: "a reason", wasClean: false})
+            expect(callback).toHaveBeenCalled()
+            const event = callback.calls.argsFor(0)[0]
+            expect(event.code).toEqual(42)
+            expect(event.reason).toEqual('a reason')
+            expect(event.wasClean).toEqual(false)
+        })
+
+        it('fires an event when opening', async () => {
+            socket.addEventListener('open', callback)
+            stubSocket.onopen({})
+            expect(callback).toHaveBeenCalled()
+        })
+
+        it('fires an open event when connecting', async () => {
+            socket.addEventListener('connecting', callback)
+            socket.open()
+            expect(callback).toHaveBeenCalled()
+        })
+
+        it('sets the binary type when opening', async () => {
+            socket.open()
+            expect(stubSocket.binaryType).toEqual('blob');
+        });
+
+        it('schedules a connection timeout when opening', async () => {
+            socket.addEventListener('close', callback)
+            stubSocket.close = callback
+            timeoutFns[2000]() // Simulate the timeout
+            expect(callback).toHaveBeenCalled()
+        });
+
+        it('does not trigger a close event when a connect times out', async () => {
+            socket.addEventListener('close', callback)
+            stubSocket.close = () => {
+                // Simulate the socket closing immediately
+                stubSocket.onclose({})
+            }
+            timeoutFns[2000]() // Simulate the timeout
+            expect(callback).not.toHaveBeenCalled()
+        });
     })
-
-    it('sends static properties when connecting', async () => {
-        stubSocket.onopen()
-        expect(sentMessages).toContain({'location.host':'localhost:12345'})
-    })
-
-    it('does not send stats until connected', async () => {
-        expect(sentMessages).toEqual([])
-    })
-
-    it('sends tracked properties when they change', async () => {
-        stubSocket.onopen()
-        stubWindow.location.hash = '#newhash'
-        eventListeners['hashchange']()
-        expect(sentMessages).toContain({'location.hash':'#newhash'})
-    })
-
-    it('sends polled properties when connected', async () => {
-        stubSocket.onopen()
-        expect(sentMessages).toContain({'navigator.cookieEnabled':true})
-    })
-
-    it('only sends polled properties when they change', async () => {
-        stubSocket.onopen()
-        sentMessages = []
-        stubWindow.navigator.cookieEnabled = false
-        intervalFn()
-        intervalFn()
-        expect(sentMessages).toEqual([{'navigator.cookieEnabled':false}])
-    })
-
-    it('evaluates any message sent', async () => {
-        stubSocket.onmessage({data: "1+1"});
-        expect(sentMessages).toEqual([{'$_':2}])
-    });
-
-    it('sends an error object if evaluation fails', async () => {
-        stubSocket.onmessage({data: "1+"})
-        let error = sentMessages[0]['$__']
-        expect(error.message).toEqual("Unexpected end of input")
-        expect(error.stack).toContain("WebSocket.ws.onmessage")
-    });
 })
--- src/control.js
+++ src/control.js
@@ -1,6 +1,166 @@
 "use strict"
 
-const control = (function () {
+const control = (() => {
+    function InternalSocket(url, windowRef) {
+        // Settings
+        const reconnectInterval = 1000
+        const maxReconnectInterval = 30000
+        const reconnectDecay = 1.5
+        const timeoutInterval = 2000
+        const binaryType = 'blob'
+
+        // Public state
+        let reconnectAttempts = 0
+        let readyState = WebSocket.CONNECTING
+        let protocol = null
+        Object.defineProperty(this, "url", {
+            value: url,
+            writable: false
+        })
+        Object.defineProperty(this, "reconnectAttempts", {
+            get: () => reconnectAttempts
+        })
+        Object.defineProperty(this, "readyState", {
+            get: () => readyState
+        })
+        Object.defineProperty(this, "protocol", {
+            get: () => protocol
+        })
+
+        // Private state
+        let self = this
+        let ws
+        let forcedClose = false
+        let timedOut = false
+        let eventTarget = document.createElement('div')
+
+        eventTarget.addEventListener('open',       function(event) { self.onopen(event); })
+        eventTarget.addEventListener('close',      function(event) { self.onclose(event); })
+        eventTarget.addEventListener('connecting', function(event) { self.onconnecting(event); })
+        eventTarget.addEventListener('message',    function(event) { self.onmessage(event); })
+        eventTarget.addEventListener('error',      function(event) { self.onerror(event); })
+
+        // Expose the API required by EventTarget
+        this.addEventListener = eventTarget.addEventListener.bind(eventTarget)
+        this.removeEventListener = eventTarget.removeEventListener.bind(eventTarget)
+        this.dispatchEvent = eventTarget.dispatchEvent.bind(eventTarget)
+
+        /**
+         * This function generates an event that is compatible with standard
+         * compliant browsers and IE9 - IE11
+         *
+         * This will prevent the error:
+         * Object doesn't support this action
+         *
+         * http://stackoverflow.com/questions/19345392/why-arent-my-parameters-getting-passed-through-to-a-dispatched-event/19345563#19345563
+         * @param s String The name that the event should use
+         * @param args Object an optional object that the event will use
+         */
+        function generateEvent(s, args) {
+            let evt = document.createEvent("CustomEvent")
+            evt.initCustomEvent(s, false, false, args)
+            return evt
+        }
+
+        this.open = function (reconnectAttempt) {
+            ws = new windowRef.WebSocket(url, [])
+            ws.binaryType = binaryType
+
+            if (!reconnectAttempt) {
+                eventTarget.dispatchEvent(generateEvent('connecting'))
+                reconnectAttempts = 0
+            }
+
+            let localWs = ws
+            let timeout = windowRef.setTimeout(function() {
+                timedOut = true
+                localWs.close()
+                timedOut = false
+            }, timeoutInterval)
+
+            ws.onopen = function(event) {
+                clearTimeout(timeout)
+                protocol = ws.protocol
+                readyState = WebSocket.OPEN
+                reconnectAttempts = 0
+                let e = generateEvent('open')
+                e.isReconnect = reconnectAttempt
+                reconnectAttempt = false
+                eventTarget.dispatchEvent(e)
+            }
+
+            ws.onclose = function(event) {
+                clearTimeout(timeout)
+                ws = null
+                if (forcedClose) {
+                    readyState = WebSocket.CLOSED
+                    eventTarget.dispatchEvent(generateEvent('close'))
+                } else {
+                    readyState = WebSocket.CONNECTING
+                    let e = generateEvent('connecting')
+                    e.code = event.code
+                    e.reason = event.reason
+                    e.wasClean = event.wasClean
+                    eventTarget.dispatchEvent(e)
+                    if (!reconnectAttempt && !timedOut) {
+                        eventTarget.dispatchEvent(generateEvent('close'))
+                    }
+
+                    let timeout = reconnectInterval * Math.pow(self.reconnectDecay, reconnectAttempts)
+                    windowRef.setTimeout(function() {
+                        reconnectAttempts++
+                        self.open(true)
+                    }, timeout > maxReconnectInterval ? maxReconnectInterval : timeout)
+                }
+            }
+            ws.onmessage = function(event) {
+                let e = generateEvent('message')
+                e.data = event.data
+                eventTarget.dispatchEvent(e)
+            }
+            ws.onerror = function(event) {
+                eventTarget.dispatchEvent(generateEvent('error'))
+            }
+        }
+
+        this.open(false)
+
+        this.send = function(data) {
+            // data is a text string, ArrayBuffer or Blob to send to the server.
+            if (ws) {
+                return ws.send(data)
+            } else {
+                throw 'INVALID_STATE_ERR : Pausing to reconnect websocket'
+            }
+        }
+
+        /**
+         * Closes the WebSocket connection or connection attempt, if any.
+         * If the connection is already CLOSED, this method does nothing.
+         */
+        this.close = function(code, reason) {
+            // Default CLOSE_NORMAL code
+            if (typeof code == 'undefined') {
+                code = 1000
+            }
+            forcedClose = true
+            if (ws) {
+                ws.close(code, reason)
+            }
+        }
+    }
+
+    InternalSocket.prototype.onopen = () => {}
+    InternalSocket.prototype.onclose = () => {}
+    InternalSocket.prototype.onconnecting = () => {}
+    InternalSocket.prototype.onmessage = () => {}
+    InternalSocket.prototype.onerror = () => {}
+
+    InternalSocket.CONNECTING = WebSocket.CONNECTING
+    InternalSocket.OPEN = WebSocket.OPEN
+    InternalSocket.CLOSING = WebSocket.CLOSING
+    InternalSocket.CLOSED = WebSocket.CLOSED
+
     function connect(config) {
         const windowRef = config.window || window
         const socket = new InternalSocket(config.url, windowRef)
@@ -92,174 +252,8 @@ const control = (function () {
         tick();
     }
 
-    function InternalSocket(url, windowRef) {
-        // Settings
-        const reconnectInterval = 1000
-        const maxReconnectInterval = 30000
-        const reconnectDecay = 1.5
-        const timeoutInterval = 2000
-        const binaryType = 'blob'
-
-        // Public state
-        let reconnectAttempts = 0
-        let readyState = WebSocket.CONNECTING
-        let protocol = null
-        Object.defineProperty(this, "url", {
-            value: url,
-            writable: false
-        })
-        Object.defineProperty(this, "reconnectAttempts", {
-            get: () => reconnectAttempts
-        })
-        Object.defineProperty(this, "readyState", {
-            get: () => readyState
-        })
-        Object.defineProperty(this, "protocol", {
-            get: () => protocol
-        })
-
-
-        // Private state
-
-        let self = this
-        let ws
-        let forcedClose = false
-        let timedOut = false
-        let eventTarget = document.createElement('div')
-
-        // Wire up "on*" properties as event handlers
-
-        eventTarget.addEventListener('open',       function(event) { self.onopen(event); })
-        eventTarget.addEventListener('close',      function(event) { self.onclose(event); })
-        eventTarget.addEventListener('connecting', function(event) { self.onconnecting(event); })
-        eventTarget.addEventListener('message',    function(event) { self.onmessage(event); })
-        eventTarget.addEventListener('error',      function(event) { self.onerror(event); })
-
-        // Expose the API required by EventTarget
-
-        this.addEventListener = eventTarget.addEventListener.bind(eventTarget)
-        this.removeEventListener = eventTarget.removeEventListener.bind(eventTarget)
-        this.dispatchEvent = eventTarget.dispatchEvent.bind(eventTarget)
-
-        /**
-         * This function generates an event that is compatible with standard
-         * compliant browsers and IE9 - IE11
-         *
-         * This will prevent the error:
-         * Object doesn't support this action
-         *
-         * http://stackoverflow.com/questions/19345392/why-arent-my-parameters-getting-passed-through-to-a-dispatched-event/19345563#19345563
-         * @param s String The name that the event should use
-         * @param args Object an optional object that the event will use
-         */
-        function generateEvent(s, args) {
-            let evt = document.createEvent("CustomEvent")
-            evt.initCustomEvent(s, false, false, args)
-            return evt
-        }
-
-        this.open = function (reconnectAttempt) {
-            ws = new windowRef.WebSocket(url, [])
-            ws.binaryType = binaryType
-
-            if (!reconnectAttempt) {
-                eventTarget.dispatchEvent(generateEvent('connecting'))
-                reconnectAttempts = 0
-            }
-
-            let localWs = ws
-            let timeout = windowRef.setTimeout(function() {
-                timedOut = true
-                localWs.close()
-                timedOut = false
-            }, timeoutInterval)
-
-            ws.onopen = function(event) {
-                clearTimeout(timeout)
-                protocol = ws.protocol
-                readyState = WebSocket.OPEN
-                reconnectAttempts = 0
-                let e = generateEvent('open')
-                e.isReconnect = reconnectAttempt
-                reconnectAttempt = false
-                eventTarget.dispatchEvent(e)
-            }
-
-            ws.onclose = function(event) {
-                clearTimeout(timeout)
-                ws = null
-                if (forcedClose) {
-                    readyState = WebSocket.CLOSED
-                    eventTarget.dispatchEvent(generateEvent('close'))
-                } else {
-                    readyState = WebSocket.CONNECTING
-                    let e = generateEvent('connecting')
-                    e.code = event.code
-                    e.reason = event.reason
-                    e.wasClean = event.wasClean
-                    eventTarget.dispatchEvent(e)
-                    if (!reconnectAttempt && !timedOut) {
-                        eventTarget.dispatchEvent(generateEvent('close'))
-                    }
-
-                    let timeout = reconnectInterval * Math.pow(self.reconnectDecay, reconnectAttempts)
-                    windowRef.setTimeout(function() {
-                        reconnectAttempts++
-                        self.open(true)
-                    }, timeout > maxReconnectInterval ? maxReconnectInterval : timeout)
-                }
-            }
-            ws.onmessage = function(event) {
-                let e = generateEvent('message')
-                e.data = event.data
-                eventTarget.dispatchEvent(e)
-            }
-            ws.onerror = function(event) {
-                eventTarget.dispatchEvent(generateEvent('error'))
-            }
-        }
-
-        this.open(false)
-
-        /**
-         * @param data a text string, ArrayBuffer or Blob to send to the server.
-         */
-        this.send = function(data) {
-            if (ws) {
-                return ws.send(data)
-            } else {
-                throw 'INVALID_STATE_ERR : Pausing to reconnect websocket'
-            }
-        }
-
-        /**
-         * Closes the WebSocket connection or connection attempt, if any.
-         * If the connection is already CLOSED, this method does nothing.
-         */
-        this.close = function(code, reason) {
-            // Default CLOSE_NORMAL code
-            if (typeof code == 'undefined') {
-                code = 1000
-            }
-            forcedClose = true
-            if (ws) {
-                ws.close(code, reason)
-            }
-        }
-    }
-
-    InternalSocket.prototype.onopen = () => {}
-    InternalSocket.prototype.onclose = () => {}
-    InternalSocket.prototype.onconnecting = () => {}
-    InternalSocket.prototype.onmessage = () => {}
-    InternalSocket.prototype.onerror = () => {}
-
-    InternalSocket.CONNECTING = WebSocket.CONNECTING
-    InternalSocket.OPEN = WebSocket.OPEN
-    InternalSocket.CLOSING = WebSocket.CLOSING
-    InternalSocket.CLOSED = WebSocket.CLOSED
-
     return {
-        connect: connect,
+        InternalSocket,
+        connect
     }
-})()
+})();
